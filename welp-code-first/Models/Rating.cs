e839193716a7c9bebe5198b49645d2aaf5f5a276
James M. Allen <james.m.allen@gmail.com>
2016-07-26 10:15:08 -0400

    Model creation

---
+++ welp-code-first/Models/Rating.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace welp_code_first.Models
+{
+    public class Rating
+    {
+        // properties
+        public int RatingID { get; set; }
+        public int UserID { get; set; }
+        public int BusinessID { get; set; }
+        public short? StarReview { get; set; }
+        public string TextReview { get; set; }
+
+        // navigation properties
+        public virtual User User { get; set; }
+        public virtual Business Business { get; set; }
+    }
+}
---
+++ welp-code-first/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace welp_code_first.Models
+{
+    public class User
+    {
+        // properties
+        public int UserID { get; set; }
+        [MaxLength(50)]
+        public string Username { get; set; }
+        [MaxLength(50)]
+        public string Password { get; set; }
+        [MaxLength(100)]
+        public string Email { get; set; }
+        [MaxLength(100)]
+        public string Location { get; set; }
+
+        // navigation properties
+        public virtual ICollection<Rating> Ratings { get; set; }
+        public virtual ICollection<Business> Businesses { get; set; }
+    }
+}
